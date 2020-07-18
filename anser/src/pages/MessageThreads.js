893ff8c682cf6e4dc8b7b28d62a0c81f09860c33
dder725 <dder725@aucklanduni.ac.nz>
2020-07-18 16:31:42 +1200

    Add redirect to a chat after clicking on a thread

--- anser/src/pages/MessageThreads.js
+++ anser/src/pages/MessageThreads.js
@@ -2,83 +2,93 @@ import Header from "../components/Header"
 import React, { Component } from 'react'
 import { Row, Button, ListGroup } from "react-bootstrap"
 import 'react-chat-elements/dist/main.css';
-import  {ChatList} from 'react-chat-elements'
+import { ChatList } from 'react-chat-elements'
 import { AvatarGenerator } from 'random-avatar-generator';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 var humanNames = require('human-names');
 export default class MessageThreads extends Component {
-     
+   state = {
+       redirect: false
+   }
+    
+    handleOnClick = () => {
+        this.setState({ redirect: true });
+    }
     render() {
-const generator = new AvatarGenerator();
-        return( <div>
-        <Header/><ChatList
-            className='chat-list'
-            dataSource={[
-                {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'What are you doing?',
-                    date: new Date(),
-                    unread: 1,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }, {
-                    avatar: generator.generateRandomAvatar(),
-                    alt: 'Reactjs',
-                    title: humanNames.maleRandom(),
-                    subtitle: 'I am ready to break the ice!',
-                    date: new Date(),
-                    unread: 2,
-                }
-            ]} /><div class="d-flex justify-content-center"><Link to='/matching'><Button>Match with new people!</Button></Link></div></div>
+        if (this.state.redirect) {
+            return <Redirect push to="/chat" />;
+        }
+        const generator = new AvatarGenerator();
+        return (<div>
+            <Header /><ChatList
+                className='chat-list'
+                onClick={this.handleOnClick}
+                dataSource={[
+                    {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'What are you doing?',
+                        date: new Date(),
+                        unread: 1,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }, {
+                        avatar: generator.generateRandomAvatar(),
+                        alt: 'Reactjs',
+                        title: humanNames.maleRandom(),
+                        subtitle: 'I am ready to break the ice!',
+                        date: new Date(),
+                        unread: 2,
+                    }
+                ]} /><div class="d-flex justify-content-center"><Link to='/matching'><Button>Match with new people!</Button></Link></div></div>
         )
     }
 }
